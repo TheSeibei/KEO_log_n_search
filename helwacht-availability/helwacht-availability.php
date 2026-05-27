b30b86e11cad9eea6cc8db7b1b459f789e3f255a
TheSeibei <sebi16b@gmail.com>
2026-05-27 10:11:25 +0200

    another test commit

--- helwacht-availability/helwacht-availability.php
+++ helwacht-availability/helwacht-availability.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Helwacht Availability
  * Description: Members can toggle availability; exposes a JSON REST endpoint for Helwacht.
- * Version: 0.3.1
+ * Version: 0.3.2
  */
 
 if (!defined('ABSPATH')) exit;
