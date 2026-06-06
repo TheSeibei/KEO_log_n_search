b12faf66b7f27eb5aa8aa3bc10363b8c98c1ee92
TheSeibei <sebi16b@gmail.com>
2026-06-06 02:43:29 +0200

    fix

--- keo-user-search-map/keo-user-search-map.php
+++ keo-user-search-map/keo-user-search-map.php
@@ -578,10 +578,11 @@ class KEO_User_Search_Map {
               card.classList.add('hws-card--clickable');
               card.addEventListener('click', function (e) {
                 if (e.target.closest('a')) return;
+                
+                closeAllMarkerPopups(businessMarkers);
 
                 if (selectedIndex === i) {
                   deselectCards();
-                  closeAllMarkerPopups(businessMarkers);
                   if (lastBounds) map.fitBounds(lastBounds, { padding: 50, maxZoom: 14 });
                 } else {
                   selectCard(i);
